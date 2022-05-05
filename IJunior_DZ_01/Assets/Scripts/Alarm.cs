bd369d61ad2693ddce4db1e1ed6339df9d3d5097
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-05-05 12:54:00 +0500

    Изменил

--- IJunior_DZ_01/Assets/Scripts/Alarm.cs
+++ IJunior_DZ_01/Assets/Scripts/Alarm.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))] 
+[RequireComponent(typeof(AudioSource))]
 
 public class Alarm : MonoBehaviour
 {
     [SerializeField] private float _speedVolumeChange;
-    
+
     private AudioSource _alarmAudio;
 
     private WaitForSeconds _waitTime = new WaitForSeconds(0.1f);
 
     private float _maxVolume = 1f;
     private float _minVolume = 0.1f;
+    private bool _swindlerInHouse;
 
     private void Start()
     {
@@ -22,16 +23,32 @@ public class Alarm : MonoBehaviour
         StartCoroutine(VolumeTuning());
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Swindler")
+        {
+            _swindlerInHouse = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Swindler")
+        {
+            _swindlerInHouse = false;
+        }
+    }
+
     private IEnumerator VolumeTuning()
     {
         while (true)
         {
-            while(_alarmAudio.volume < _maxVolume)
+            if(_swindlerInHouse)
             {
                 _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _maxVolume, _speedVolumeChange * Time.deltaTime);
                 yield return _waitTime;
             }
-            while(_alarmAudio.volume > _minVolume)
+            else
             {
                 _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _minVolume, _speedVolumeChange * Time.deltaTime);
                 yield return _waitTime;
