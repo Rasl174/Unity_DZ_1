5afa52e846f6f2b112168fdc1393176c25162b90
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-05-05 16:00:56 +0500

    Изменил

--- IJunior_DZ_01/Assets/Scripts/Alarm.cs
+++ IJunior_DZ_01/Assets/Scripts/Alarm.cs
@@ -8,51 +8,47 @@ public class Alarm : MonoBehaviour
     [SerializeField] private float _speedVolumeChange;
 
     private AudioSource _alarmAudio;
-
     private WaitForSeconds _waitTime = new WaitForSeconds(0.1f);
+    private Coroutine _corutineVolumeTuning;
 
-    private float _maxVolume = 1f;
-    private float _minVolume = 0.1f;
-    private bool _swindlerInHouse;
+    private float _minVolume = 0f;
 
     private void Start()
     {
         _alarmAudio = GetComponent<AudioSource>();
         _alarmAudio.volume = _minVolume;
-
-        StartCoroutine(VolumeTuning());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Swindler")
+        if(collision.TryGetComponent<Swindler>(out Swindler swindler))
         {
-            _swindlerInHouse = true;
+            if (_corutineVolumeTuning != null)
+            {
+                StopCoroutine(_corutineVolumeTuning);
+            }
+            _corutineVolumeTuning = StartCoroutine(VolumeTuning(1));
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Swindler")
+        if (collision.TryGetComponent<Swindler>(out Swindler swindler))
         {
-            _swindlerInHouse = false;
+            if(_corutineVolumeTuning != null)
+            {
+                StopCoroutine(_corutineVolumeTuning);
+            }
+            _corutineVolumeTuning = StartCoroutine(VolumeTuning(0));
         }
     }
 
-    private IEnumerator VolumeTuning()
+    private IEnumerator VolumeTuning(float target)
     {
         while (true)
         {
-            if(_swindlerInHouse)
-            {
-                _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _maxVolume, _speedVolumeChange * Time.deltaTime);
-                yield return _waitTime;
-            }
-            else
-            {
-                _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _minVolume, _speedVolumeChange * Time.deltaTime);
-                yield return _waitTime;
-            }
+            _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, target, _speedVolumeChange * Time.deltaTime);
+            yield return _waitTime;
         }
     }
 }
